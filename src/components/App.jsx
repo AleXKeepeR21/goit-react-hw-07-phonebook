9931a34c26bbbef8e2dffb55717bd871dc885186
AleXKeepeR21 <106617654+AleXKeepeR21@users.noreply.github.com>
2023-01-28 14:40:34 +0200

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,53 @@
-export const App = () => {
+import { useEffect, useState } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+import { useSelector } from 'react-redux';
+
+export function App() {
+  const [contacts, setContacts] = useState([]);
+  // const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const getContacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(getContacts);
+
+    if (parsedContacts !== 0) {
+      setContacts(parsedContacts);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const contactsList = useSelector(state => state.contacts.contacts);
+
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
         justifyContent: 'center',
+        flexDirection: 'column',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        fontSize: 24,
+        color: '#010101',
+        textShadow: 'rgb(232, 216, 137) 1px 0 10px',
+        backgroundColor: 'rgba(0, 0, 255, 0.032)',
       }}
     >
-      React homework template
+      <>
+        {contactsList.length > 0 && (
+          <h3>You have {contactsList.length} contast(s)</h3>
+        )}
+      </>
+
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
     </div>
   );
-};
+}
