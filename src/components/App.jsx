82fa056fc576a025a7a6520ac863bccf96fce3b6
AleXKeepeR21 <106617654+AleXKeepeR21@users.noreply.github.com>
2023-02-03 00:25:55 +0200

    add code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,37 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { getError, getIsLoading } from 'redux/selector';
+import { fetchContacts } from 'redux/operations';
 
 export function App() {
-  const [contacts, setContacts] = useState([]);
+  // const [contacts, setContacts] = useState([]);
   // const [filter, setFilter] = useState('');
 
+  // useEffect(() => {
+  //   const getContacts = localStorage.getItem('contacts');
+  //   const parsedContacts = JSON.parse(getContacts);
+
+  //   if (parsedContacts !== 0) {
+  //     setContacts(parsedContacts);
+  //   }
+  // }, []);
+
+  // useEffect(() => {
+  //   localStorage.setItem('contacts', JSON.stringify(contacts));
+  // }, [contacts]);
+
+  const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+
   useEffect(() => {
-    const getContacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(getContacts);
-
-    if (parsedContacts !== 0) {
-      setContacts(parsedContacts);
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const contactsList = useSelector(state => state.contacts.contacts);
+    dispatch(fetchContacts());
+  }, [dispatch]);
+  // const contactsList = useSelector(state => state.contacts.contacts);
 
   return (
     <div
@@ -37,16 +46,18 @@ export function App() {
         backgroundColor: 'rgba(0, 0, 255, 0.032)',
       }}
     >
-      <>
+      {/* <>
         {contactsList.length > 0 && (
           <h3>You have {contactsList.length} contast(s)</h3>
         )}
-      </>
+      </> */}
 
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
       <Filter />
+      {/* {isLoading && !error && <b>Request in progress...</b>} */}
+      {isLoading && !error}
       <ContactList />
     </div>
   );
--- src/features/Contacts/selector.js
+++
@@ -1,6 +0,0 @@
-export const filteredContacts = state => {
-  const normalizedFilter = state.contacts.filter.toLowerCase();
-  return state.contacts.contacts.filter(contact => {
-    return contact.name.toLowerCase().includes(normalizedFilter);
-  });
-};
